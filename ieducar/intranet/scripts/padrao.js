56ef0c6112c23a0a6d06ba23904d41ec20209b53
Alexia Nunes <alexiiianunes@gmail.com>
2022-11-10 21:13:57 -0300

    hotfix: tamanho modal pesquisar valores

--- ieducar/intranet/scripts/padrao.js
+++ ieducar/intranet/scripts/padrao.js
@@ -481,10 +481,10 @@ function pesquisa_valores_popless(caminho, campo)
   let div = 'div_dinamico_' + new_id;
 
   if (host.indexOf('?') === -1) {
-    showExpansivel(500, 500, '<iframe src="' + host + '?campo=' + campo + '&div=' + div + '&popless=1" frameborder="0" height="100%" width="500" marginheight="0" marginwidth="0" name="temp_win_popless"></iframe>', 'Pesquisa de valores');
+    showExpansivel(1024, 480, '<iframe src="' + host + '?campo=' + campo + '&div=' + div + '&popless=1" frameborder="0" height="100%" width="100%" marginheight="0" marginwidth="0" name="temp_win_popless"></iframe>', 'Pesquisa de valores');
   }
   else {
-    showExpansivel(500, 500, '<iframe src="' + host + '&campo=' + campo + '&div=' + div + '&popless=1" frameborder="0" height="100%" width="500" marginheight="0" marginwidth="0" name="temp_win_popless"></iframe>', 'Pesquisa de valores');
+    showExpansivel(1024, 480, '<iframe src="' + host + '&campo=' + campo + '&div=' + div + '&popless=1" frameborder="0" height="100%" width="100%" marginheight="0" marginwidth="0" name="temp_win_popless"></iframe>', 'Pesquisa de valores');
   }
 }
 
