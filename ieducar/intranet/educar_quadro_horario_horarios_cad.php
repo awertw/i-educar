fcddc26941337780f830e7bec2d4ff8707af9b4e
Alexia Nunes <alexiiianunes@gmail.com>
2022-12-22 18:21:16 -0300

    hotfix: [EDU-106] Adicionando frequencia_aluno na exclusão e corrigindo quantidade de faltas

--- ieducar/intranet/educar_quadro_horario_horarios_cad.php
+++ ieducar/intranet/educar_quadro_horario_horarios_cad.php
@@ -238,7 +238,7 @@ return new class extends clsCadastro {
         $turma = new clsPmieducarTurma($this->ref_cod_turma);
         $turma = $turma->detalhe();
 
-        if (in_array($turma['etapa_educacenso'], [14, 15, 16, 17, 18, 24, 69])) {
+        if (in_array($turma['etapa_educacenso'], [14, 15, 16, 17, 18, 24, 69, 22])) {
             $this->registra_diario_individual = isset($this->registra_diario_individual) ? 1 : 0;
 
             $this->campoCheck(
