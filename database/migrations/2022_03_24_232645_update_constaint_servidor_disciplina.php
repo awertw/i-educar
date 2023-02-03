f07085bb6e878647a7f9ee810733fd63bcaca3fe
xlcn2 <lucianopeas@gmail.com>
2023-02-03 12:26:06 -0300

    upgrade

--- database/migrations/2022_03_24_232645_update_constaint_servidor_disciplina.php
+++ database/migrations/2022_03_24_232645_update_constaint_servidor_disciplina.php
@@ -16,18 +16,7 @@ class UpdateConstaintServidorDisciplina extends Migration
      */
     public function up()
     {
-        $columns = [
-            'ref_cod_disciplina',
-            'ref_ref_cod_instituicao',
-            'ref_cod_servidor',
-            'ref_cod_curso',
-            'ref_cod_funcao'
-        ];
-       
-
-        //$this->dropPrimaryKeyIn('pmieducar', 'servidor_disciplina', 'servidor_disciplina_pkey');
-        $this->createConstraint('pmieducar.servidor_disciplina', $columns, 'servidor_disciplina_pkey');
-    }
+     }
 
     /**
      * Reverse the migrations.
@@ -35,15 +24,5 @@ class UpdateConstaintServidorDisciplina extends Migration
      * @return void
      */
     public function down()
-    {
-        $columns = [
-            'ref_cod_disciplina',
-            'ref_ref_cod_instituicao',
-            'ref_cod_servidor',
-            'ref_cod_curso'
-        ];
-
-        //$this->dropPrimaryKeyIn('pmieducar', 'servidor_disciplina', 'servidor_disciplina_pkey');
-        $this->createConstraint('pmieducar.servidor_disciplina', $columns, 'servidor_disciplina_pkey');
-    }
+    {}
 }
--- database/migrations/2022_04_26_093217_add_race_view_export_student.php
+++ database/migrations/2022_04_26_093217_add_race_view_export_student.php
@@ -9,19 +9,11 @@ class AddRaceViewExportStudent extends Migration
 
     public function up()
     {
-        $this->dropView('public.exporter_social_assistance');
-        $this->dropView('public.exporter_student');
-
-        $this->createView('public.exporter_student', '2022-04-26');
-        $this->createView('public.exporter_social_assistance', '2020-05-07');
+       
     }
 
     public function down()
     {
-        $this->dropView('public.exporter_social_assistance');
-        $this->dropView('public.exporter_student');
-
-        $this->createView('public.exporter_student', '2021-07-19');
-        $this->createView('public.exporter_social_assistance', '2020-05-07');
+     
     }
 }
