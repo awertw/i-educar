ad3319a77e9961889bbc3227c704e931e8f5484a
Alexia Nunes <alexiiianunes@gmail.com>
2023-02-04 19:45:13 -0300

    Adicionar/Editar Falta Atraso - Coordenador

---
+++ database/migrations/2023_01_27_070559_add_columns_feature_coordenacao_atraso_falta_servidor.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pmieducar.quadro_horario_horarios', function (Blueprint $table) {
+            $table->increments('ref_cod_quadro_horario_horarios');
+        });
+
+        Schema::table('pmieducar.falta_atraso', function (Blueprint $table) {
+            $table->unsignedBigInteger('ref_cod_curso')->nullable();
+            $table->unsignedBigInteger('ref_cod_serie')->nullable();
+            $table->unsignedBigInteger('ref_cod_turma')->nullable();
+            $table->unsignedBigInteger('ref_cod_componente_curricular')->nullable();
+            $table->text('observacao')->nullable();
+            $table->integer('ano')->nullable();
+            $table->integer('qtd_aulas')->default(0);
+        });
+
+        Schema::create('pmieducar.falta_atraso_horarios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ref_cod_falta_atraso');
+            $table->unsignedBigInteger('ref_cod_quadro_horario_horarios');
+            $table->string('aulas')->default('1')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/migrations/2023_02_04_193835_add_menu_coordenacao_falta_atraso.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('public.menus')->insert(
+            array(
+                'id' => 3956,
+                'parent_id' => 393,
+                'title' => 'Cadastro Falta/Atraso Professor',
+                'description' => 'Cadastro Falta/Atraso Professor',
+                'link' => '/intranet/educar_coordenacao_falta_atraso_professor_lst.php',
+                'icon' => NULL,
+                'order' => 3,
+                'type' => 2,
+                'process' => 58,
+                'old' => NULL,
+                'parent_old' => NULL,
+                'active' => true,
+                'created_at' => NULL,
+                'updated_at' => NULL
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
