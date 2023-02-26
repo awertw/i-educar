fbe81d1adf60a2488cb65dce6d450a2f4dd87ecf
Alexia Nunes <alexiiianunes@gmail.com>
2023-02-26 17:14:56 -0300

    migration drop columns

--- database/migrations/2023_02_26_183905_alter_columns_gestor_responsavel_contabil_on_instituicao.php
+++ database/migrations/2023_02_26_183905_alter_columns_gestor_responsavel_contabil_on_instituicao.php
@@ -15,8 +15,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('pmieducar.instituicao', function (Blueprint $table) {
-            DB::statement('ALTER TABLE pmieducar.instituicao ALTER COLUMN responsavel_contabil TYPE integer USING (responsavel_contabil)::integer');
-            DB::statement('ALTER TABLE pmieducar.instituicao ALTER COLUMN gestor TYPE integer USING (gestor)::integer');
+            $table->integer('responsavel_contabil')->nullable();
+            $table->integer('gestor')->nullable();
         });
     }
 
@@ -26,10 +26,5 @@ return new class extends Migration
      * @return void
      */
     public function down()
-    {
-        Schema::table('pmieducar.instituicao', function (Blueprint $table) {
-            $table->string('responsavel_contabil')->nullable()->change();
-            $table->string('gestor')->nullable()->change();
-        });
-    }
+    {}
 };
