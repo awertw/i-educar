715ed71bdfa6ddc48b27e216d0a0deb9c8510211
Alexia Nunes <alexiiianunes@gmail.com>
2023-03-10 21:00:37 -0300

    fix: desfazendo testes

--- app/Queries/SagresExport/ExportQuery.php
+++ app/Queries/SagresExport/ExportQuery.php
@@ -9,39 +9,10 @@ class ExportQuery
 {
     public function getInstitution(int $institutionId): LegacyInstitution
     {
-        $teste = LegacyInstitution::where('cod_instituicao', $institutionId)->with(['schools' => function($query) {
-            $query->where('cod_escola', 8);
-        }])->get();
-
-//        return LegacyInstitution::find($institutionId);
-        return $teste[0];
+        return LegacyInstitution::find($institutionId);
     }
 
     public function getTotalEnrollmentAbsencesByFrequency(int $enrollmentId, string $startDate, string $endDate): int
-    {
-        $studentAbsencesFrequencys = FrequenciaAluno::where('ref_cod_matricula', $enrollmentId)->with(['frequency' => function($query) use($startDate, $endDate) {
-            $query->whereDate('frequencia.data', '<=', $endDate);
-            $query->whereDate('frequencia.data', '>=', $startDate);
-        }])->get();
-
-        $qtdAbsences = 0;
-        foreach ($studentAbsencesFrequencys as $studentAbsencesFrequency) {
-            $absencesClass = $studentAbsencesFrequency['aulas_faltou'];
-
-            if ($absencesClass == 'undefined' || empty($absencesClass)) {
-                $qtdAbsences++;
-                continue;
-            }
-
-            $absencesClassExplode = explode(',', $absencesClass);
-            $qtdAbsences = count($absencesClassExplode);
-
-        }
-
-        return $qtdAbsences;
-    }
-
-    public function getSchoolClassSnackMenu(int $enrollmentId, string $startDate, string $endDate): int
     {
         $studentAbsencesFrequencys = FrequenciaAluno::where('ref_cod_matricula', $enrollmentId)->with(['frequency' => function($query) use($startDate, $endDate) {
             $query->whereDate('frequencia.data', '<=', $endDate);
